fb5d4eb5f5cc2d4437091527c837906bad8125d1
adovg <kroatoan@i.ua>
2023-11-13 23:13:09 +0200

    gallery vol 1

--- src/main.js
+++ src/main.js
@@ -104,10 +104,11 @@ breakpoints: {
 
 const galleryItems = document.querySelectorAll(".gallery__item");
 
-if (galleryItems.length > 0) {
+if (galleryItems.length !== 0) {
 	galleryItems.forEach(item => {
 		new Swiper(item, {
 			slidesPerView: 1, 
+      autoHeight: false,
 			autoplay: {
 				delay: 5000,
 			},
@@ -116,4 +117,19 @@ if (galleryItems.length > 0) {
 	})
 }
  
-};
+
+
+// const galleryItems = new Swiper('.gallery__item', {
+//   loop: true,
+//   spaceBetween: 10,
+//     autoHeight: false,
+//   slidesPerView: 1,
+//   autoplay: {
+//     delay: 5000,
+//   },
+
+// });
+
+
+}
+
