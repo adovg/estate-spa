b8fd3bc8393b14f3c8e349b3c078c946962c16a6
adovg <kroatoan@i.ua>
2023-11-15 22:38:52 +0200

    begin section

--- src/main.js
+++ src/main.js
@@ -102,33 +102,33 @@ breakpoints: {
 });
 
 
-const galleryItems = document.querySelectorAll(".gallery__item");
+// const galleryItems = document.querySelectorAll(".gallery__item");
 
-if (galleryItems.length !== 0) {
-	galleryItems.forEach(item => {
-		new Swiper(item, {
-			slidesPerView: 1, 
-      autoHeight: false,
-			autoplay: {
-				delay: 5000,
-			},
-			effect: 'fade',
-		})
-	})
-}
+// if (galleryItems.length > 0) {
+// 	galleryItems.forEach(item => {
+// 		new Swiper(item, {
+// 			slidesPerView: 1, 
+//       autoHeight: false,
+// 			autoplay: {
+// 				delay: 5000,
+// 			},
+// 			effect: 'fade',
+// 		})
+// 	})
+// }
  
 
 
-// const galleryItems = new Swiper('.gallery__item', {
-//   loop: true,
-//   spaceBetween: 10,
-//     autoHeight: false,
-//   slidesPerView: 1,
-//   autoplay: {
-//     delay: 5000,
-//   },
+const galleryItems = new Swiper('.gallery__item', {
+  loop: true,
+  spaceBetween: 10,
+    autoHeight: false,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 5000,
+  },
 
-// });
+});
 
 
 }
